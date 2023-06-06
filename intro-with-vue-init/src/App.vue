5f77706d44400e2c05745241062d6c095c770ad1
wilbur <wilbur@wilbur.com>
2023-06-06 15:54:27 -0400

    - including version with vue init

---
+++ intro-with-vue-init/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    // name: 'App',
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<template>
+  <button v-on:click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<style>
+  #app {
+    display: grid;
+    place-content: center;
+    height: 100vh;
+  }
+
+  button {
+    font-size: 2rem;
+  }
+</style>
