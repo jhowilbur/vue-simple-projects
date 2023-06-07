9e2737207aac926072cf7093f4ef8f60a988c830
wilbur <wilbur@wilbur.com>
2023-06-07 05:45:12 -0400

    - details about v-model

--- intro-with-vue-init/src/components/Github.vue
+++ intro-with-vue-init/src/components/Github.vue
@@ -9,12 +9,12 @@
     company: '',
     bio: '',
     avatar_url: '',
-    // searchInput: '',
     repos: []
   })
 
-  async function fetchSearchUser() {
-    // const res = await fetch(`https://api.github.com/users/${state.searchInput}`);
+  async function fetchSearchUser(event) {
+    event.preventDefault();
+
     const res = await fetch(`https://api.github.com/users/${searchInput.value}`); //to be able to manipulate the value of the ref we need to use .value
     const data = await res.json();
 
@@ -39,6 +39,13 @@
         ? `${state.repos.length}`
         : `No repositories in ${state.name}`
   })
+
+  const searchingUser = computed(() => { // we can do that using v-model because will generate errors
+    return searchInput.value === ''
+        ? `Waiting for search...`
+        : `<p>Searching: <strong>https://api.github.com/users/${searchInput.value}</strong> </p>`
+        // : `<p>Searching: <strong>https://api.github.com/users/{{ searchInput }}</strong> </p>`
+  })
 </script>
 
 <template>
@@ -49,13 +56,25 @@
         <h2>GitHub User Data</h2>
         <hr>
 
-<!--        <input class="centered" type="text" v-model="state.searchInput">-->
-        <input class="centered" type="text" v-model="searchInput">
-        <br>
-        <button type="button" class="btn btn-primary" @click="fetchSearchUser">
-          Search User
-        </button>
-        <br><br>
+        <!-- v-html is a directive that allows us to pass HTML through to our templates. -->
+<!--        <div v-html="searchingUser"></div>-->
+
+        <p>
+          Searching:<br>
+          <strong v-if="searchInput !== '' ">
+            https://api.github.com/users/{{ searchInput }}
+          </strong>
+        </p>
+        <form @submit="fetchSearchUser">
+          <!-- v-model is a directive that allows us to create two-way data bindings on form input, textarea, and select elements. -->
+          <input class="centered" type="text" v-model="searchInput">
+
+          <!-- v-model.lazy change instantaneously -->
+<!--          <input class="centered" type="text" v-model.lazy="searchInput">-->
+          <br>
+          <button class="btn btn-primary"> Search User </button>
+          <br><br>
+        </form>
 
         <div v-if=" state.login !== '' ">
           <img :src="state.avatar_url">
